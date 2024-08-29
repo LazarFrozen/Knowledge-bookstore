3c7c2c2e757e24697d593fc8bc3c34ccfd0a0ada
Lazar Popovic <lazarpopovicgaming@gmail.com>
2024-08-29 20:11:04 +0200

    Fixed page not found

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Work in progress:
 2. Responsiv design
 3. Dark mode
 4. Log-in
-5. Page not found
+5. Page not found (done)
 
 Knwoledge Bookstore is a full stack web shop done with React for a frontend with a Tailwind (CSS), (Java) Spring Boot for a backend and a MySQL for a databases.
--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import PrimaryButton from "../ui/PrimaryButton";
+
 function PageNotFound() {
-  return <section>PageNotFound</section>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="flex h-svh justify-center bg-primaryColor pt-20">
+      <div className="flex flex-col items-center">
+        <h1 className="text-6xl">Error: page not found</h1>
+        <img src="Knowledge-logo.png" alt="Logo" className="pt-5" />
+        <PrimaryButton content="Home page" onClick={() => navigate("/home")} />
+      </div>
+    </section>
+  );
 }
 
 export default PageNotFound;
